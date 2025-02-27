ee6108443208ed0f635468d357e9ad68505a493e
Thomas Kroes <tkroes81@gmail.com>
2025-02-27 12:11:48 +0100

    Removed redundant override

--- src/ParallelCoordinatesPlugin.h
+++ src/ParallelCoordinatesPlugin.h
@@ -124,13 +124,6 @@ class ParallelCoordinatesPluginFactory : public ViewPluginFactory
 public:
     ParallelCoordinatesPluginFactory(void) {}
     ~ParallelCoordinatesPluginFactory(void) override {}
-    
-    /**
-     * Get plugin icon
-     * @param color Icon color for flat (font) icons
-     * @return Icon
-     */
-    QIcon getIcon(const QColor& color = Qt::black) const override;
 
     ViewPlugin* produce() override;
 
